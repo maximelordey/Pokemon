ff573cf5bce16b7c5762e1d50d8b6c5d7eb77ea5
QuSibue <quentin.sibue@live.fr>
2020-01-22 16:23:22 +0100

    first commit from Windows10 x64 with VSC2017 and SDL enabled

---
+++ Pokemon/Pokemon/Pokemon.cpp
@@ -0,0 +1,42 @@
+// Pokemon.cpp : Defines the entry point for the application.
+//
+
+#include "framework.h"
+#include "Pokemon.h"
+#include <iostream>
+#include <SDL.h>
+
+const int WIDTH = 800, HEIGHT = 600;
+
+int main(int argc, char *argv[])
+{
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	SDL_Window *window = SDL_CreateWindow("Hello SDL World for Pokemon", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
+
+	// Check that the window was successfully created
+	if (NULL == window)
+	{
+		// In the case that the window could not be made...
+		std::cout << "Could not create window: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+
+	SDL_Event windowEvent;
+
+	while (true)
+	{
+		if (SDL_PollEvent(&windowEvent))
+		{
+			if (SDL_QUIT == windowEvent.type)
+			{
+				break;
+			}
+		}
+	}
+
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return EXIT_SUCCESS;
+}
