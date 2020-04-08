5b8a2ecbb9dd3f8846c27344dc1c05dc6eebe5df
QuSibue <quentin.sibue@live.fr>
2020-04-08 13:04:11 +0200

    fix SDL_mixer lol

--- Pokemon/Pokemon/Pokemon.cpp
+++ Pokemon/Pokemon/Pokemon.cpp
@@ -5,11 +5,17 @@
 #include "Pokemon.h"
 #include <iostream>
 #include <SDL.h>
+#include <SDL_mixer.h>
+
+#define WAV_PATH "C:\\Users\\quent\\Documents\\Polytech\\Annee3\\projetPerso\\Sons\\musique.mp3"
+#define WAV2_PATH "C:\\Users\\quent\\Documents\\Polytech\\Annee3\\projetPerso\\Sons\\musique.mp3"
 
 const int WIDTH = 800, HEIGHT = 600;
 
 int main(int argc, char *argv[])
 {
+
+	std::cout << "COUCOU" << std::endl;
 	SDL_Init(SDL_INIT_EVERYTHING);
 
 	SDL_Window *window = SDL_CreateWindow("Hello SDL World for Pokemon", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
@@ -24,7 +30,7 @@ int main(int argc, char *argv[])
 
 	SDL_Event windowEvent;
 
-	while (true)
+	/*while (true)
 	{
 		if (SDL_PollEvent(&windowEvent))
 		{
@@ -33,8 +39,45 @@ int main(int argc, char *argv[])
 				break;
 			}
 		}
+	}*/
+
+
+	Mix_Music *wave = NULL;
+	Mix_Music *wave2 = NULL;
+
+	if (Mix_OpenAudio(22050, MIX_DEFAULT_FORMAT, 2, 4096) != 0) {
+		std::cout << "Impossible d'ouvrir mixer " << SDL_GetError() << std::endl;
+		return 1;
 	}
 
+	wave = Mix_LoadMUS(WAV_PATH);
+	wave2 = Mix_LoadMUS(WAV2_PATH);
+
+	std::cout << wave << std::endl;
+
+	Mix_VolumeMusic(MIX_MAX_VOLUME / 6);
+
+	if (wave == NULL || wave2 == NULL) {
+		std::cout << "Impossible de chargé un fichier .ogg " << SDL_GetError() << std::endl;
+		return 1;
+	}
+
+	if (Mix_PlayMusic(wave, 1) != 0) {
+		std::cout << "Impossible de jouer le fichier " << SDL_GetError() << std::endl;
+		return 1;
+	}
+
+	SDL_Delay(5000);
+
+	if (Mix_PlayMusic(wave2, 1) != 0) {
+		std::cout << "Impossible de jouer le fichier " << SDL_GetError() << std::endl;
+		return 1;
+	}
+
+	while (Mix_PlayingMusic());
+
+	Mix_FreeMusic(wave);
+	Mix_FreeMusic(wave2);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 
