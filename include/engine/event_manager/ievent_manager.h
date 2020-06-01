8c38b38d4e67dac40be05579b8bd6492b96b5273
mlordey <maximelordey>
2020-06-01 17:46:39 +0200

    include in file ievent_manager.h added

--- include/engine/event_manager/ievent_manager.h
+++ include/engine/event_manager/ievent_manager.h
@@ -1,10 +1,12 @@
 #ifndef IEVENT_MANAGER_H
 #define IEVENT_MANAGER_H
 
+include "ievent.h"
+
 class IEventManager {
 	public:
 		virtual bool hasEventPending() const = 0;
-		virtual const IEvent getEventPending() const = 0;
+		virtual IEvent getEventPending() const = 0;
 }
 
 #endif
