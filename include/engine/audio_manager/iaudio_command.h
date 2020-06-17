fa07f01a34b1c2163f60b08aab9cf95b41993cce
mlordey <maximelordey>
2020-06-17 20:22:52 +0200

    correcting indentation

--- include/engine/audio_manager/iaudio_command.h
+++ include/engine/audio_manager/iaudio_command.h
@@ -2,12 +2,12 @@
 #define IAUDIO_COMMAND_H
 
 class IAudioCommand {
-		virtual void pause() = 0;
+	virtual void pause() = 0;
+	
+    virtual void unpause() = 0;
 		
-        virtual void unpause() = 0;
+    virtual void stopMusic() = 0;		
 		
-        virtual void stopMusic() = 0;		
-		
-        virtual bool isPlaying() const = 0;
+    virtual bool isPlaying() const = 0;
 }
 #endif
