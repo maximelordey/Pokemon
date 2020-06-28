e8234ffa556ba3d56daebb69fbd5946dac0584aa
mlordey <maximelordey>
2020-06-28 21:27:19 +0200

    change from defaultPoint to sdlPoint.

--- include/engine/utils/sdlpoint.h
+++ include/engine/utils/sdlpoint.h
@@ -2,11 +2,12 @@
 #define DEFAULT_POINT_H
 
 #include "ipoint.h"
+#include <SDL.h>
 
-class DefaultPoint : public IPoint {
+class SdlPoint : public IPoint {
 	public:
-        DefaultPoint(int32_t x, int32_t y);
-        ~DefaultPoint();
+    SdlPoint(int32_t x, int32_t y);
+    ~SdlPoint();
 
 		int32_t getX() const override;
 		int32_t getY() const override;
@@ -14,9 +15,9 @@ class DefaultPoint : public IPoint {
 		void move(int32_t x, int32_t y) override;
 		void translate(int32_t dx, int32_t dy) override;
 
-    public:
-        uint32_t _x;
-        uint32_t _y;
+  public:
+    SDL_Point* _point;  
+
 };
 
 #endif
