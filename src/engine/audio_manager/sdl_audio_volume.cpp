aeead336f6ad995e2bcf4b0c2154016a1294c312
mlordey <maximelordey>
2020-07-10 00:52:16 +0200

    add newline to end of file.

--- src/engine/audio_manager/sdl_audio_volume.cpp
+++ src/engine/audio_manager/sdl_audio_volume.cpp
@@ -1,7 +1,7 @@
 #include "sdl_audio_volume.h"
 
 const int32_t SdlAudioVolume::GET_VOLUME = -1;
-const int32_t SdlAudioVolume::MAX_VOLUME = 128;
+const int32_t SdlAudioVolume::MAX_VOLUME = MIX_MAX_VOLUME;
 const int32_t SdlAudioVolume::MIN_VOLUME = 0;
 
 
