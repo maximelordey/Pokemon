b067458fb82232e02841ce9a8982e7101c27add1
mlordey <maximelordey>
2020-08-06 18:09:25 +0200

    texture's copy constructor/operator done.

--- engine/include/video/texture.h
+++ engine/include/video/texture.h
@@ -3,14 +3,22 @@
 
 #include <string>
 #include <SDL.h>
+
 #include "itexture.h"
+#include "rectangle.h"
 
 class Texture: public ITexture {
 	public:
+		Texture();
 		Texture(const std::string &path);
+		Texture(const Texture &texture);
+		Texture(Texture &&texture);
 		~Texture();
 
 		SDL_Surface* getSurface() const;
+
+		Texture& operator=(const Texture &texture);
+		Texture& operator=(Texture &&texture);
 
 	private: 
 		SDL_Surface* _surface;
--- engine/src/video/texture.cpp
+++ engine/src/video/texture.cpp
@@ -1,9 +1,21 @@
 #include "texture.h"
 #include <SDL_image.h>
 
+Texture::Texture() {
+
+}
 
 Texture::Texture(const std::string& path) {
 	_surface = IMG_Load(path.c_str());	
+}
+
+Texture::Texture(const Texture &texture) {
+	_surface = SDL_ConvertSurface(texture._surface,texture._surface->format,0);
+}
+
+Texture::Texture(Texture &&texture) {
+	_surface = texture._surface;
+	texture._surface = nullptr;
 }
 
 Texture::~Texture() {
@@ -13,4 +25,21 @@ Texture::~Texture() {
 
 SDL_Surface* Texture::getSurface() const {
 	return _surface;
+}
+
+Texture& Texture::operator=(const Texture &texture) {
+	if (this != &texture) {
+	SDL_FreeSurface(_surface);
+	_surface = SDL_ConvertSurface(texture._surface,texture._surface->format,0);
+	}
+	return *this;
+}
+
+Texture& Texture::operator=(Texture &&texture) {
+	if (this != &texture) {
+		SDL_FreeSurface(_surface);
+		_surface = texture._surface;
+		texture._surface = nullptr;
+	}
+	return *this;
 }
