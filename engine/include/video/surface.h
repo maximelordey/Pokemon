d02cb573dbb95dc9669e6015d491d14ca87a333e
mlordey <maximelordey>
2020-10-04 23:04:14 +0200

    add blitting and getSurface function to Surface.h

--- engine/include/video/surface.h
+++ engine/include/video/surface.h
@@ -3,18 +3,25 @@
 
 #include <string>
 #include "SDL.h"
+#include "rectangle.h"
 
 class Surface {
 	public:
 		Surface();
-		Surface(std::string &path);
-		Surface(const Surface &surface);
-		Surface(Surface &&surface);
+		Surface(SDL_Surface& surface);
+		Surface(std::string& path);
+		Surface(const Surface& surface);
+		Surface(Surface&& surface);
 		~Surface();
 
+		void setFormat(const SDL_PixelFormat& pixelFormat);
+		SDL_PixelFormat& getFormat() const;
 
-		Surface &operator=(const Surface &surface);
-		Surface &operator=(Surface &&surface);
+		Surface& blitting(const Surface& surface,const Rectangle& area) const;
+		const SDL_Surface& getSurface() const;
+
+		Surface& operator=(const Surface &surface);
+		Surface& operator=(Surface &&surface);
 
 	private:
 		SDL_Surface *_surface;
--- engine/src/video/surface.cpp
+++ engine/src/video/surface.cpp
@@ -5,6 +5,10 @@ Surface::Surface() {
 
 }
 
+Surface::Surface(SDL_Surface &surface) {
+	_surface = &surface;
+}
+
 Surface::Surface(std::string &path) {
 	_surface = IMG_Load(path.c_str());
 }
@@ -19,6 +23,24 @@ Surface::Surface(Surface &&surface) {
 
 Surface::~Surface() {
 	SDL_FreeSurface(_surface);
+}
+
+void Surface::setFormat(const SDL_PixelFormat &pixelFormat) {
+	SDL_Surface* newSurface = SDL_ConvertSurface(_surface, &pixelFormat, 0 );
+	SDL_FreeSurface(_surface);
+	_surface = newSurface;
+}
+
+SDL_PixelFormat &Surface::getFormat() const {
+	return *(_surface->format);
+}
+
+Surface& blitting(const Surface& surface,const Rectangle& area) const {
+
+}
+
+const SDL_Surface& getSurface() const {
+	
 }
 
 Surface &Surface::operator=(const Surface &surface) {
