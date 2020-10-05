0122c3e30d4ebe3df2de3ba08677d00513085867
mlordey <maximelordey>
2020-10-05 14:11:14 +0200

    correct Line constructor

--- engine/src/utils/line.cpp
+++ engine/src/utils/line.cpp
@@ -1,11 +1,11 @@
 #include "line.h"
 
 Line::Line() : 
-	_x1(0), _x2(0), _y1(0), _y2(0)
+	_x1(0), _y1(0), _x2(0), _y2(0)
 {}
 
 Line::Line(int32_t x1, int32_t y1, int32_t x2, int32_t y2) : 
-	_x1(x1), _x2(x2), _y1(y1), _y2(y2) 
+	_x1(x1), _y1(y1), _x2(x2), _y2(y2) 
 {}
 
 Line::Line(const Line &line) {
