7027af40521d886c94c887334a8a0c1cec831cb0
mlordey <maximelordey>
2020-11-23 13:11:22 +0100

    Gauge constructor(s). Gauge copy/move constructor(s) + operator(s). Gauge methods. Gauge destructor.

---
+++ game/include/utils/gauge.h
@@ -0,0 +1,33 @@
+#ifndef GAUGE_H
+#define GAUGE_H
+
+#include <cstdint>
+
+class Gauge {
+	public:
+		Gauge(uint32_t capacity);
+		Gauge(uint32_t capacity, uint32_t value);
+		Gauge(const Gauge &gauge);
+		Gauge(Gauge &&gauge);
+		~Gauge();
+
+		bool isEmpty() const;
+		bool isFull() const;
+		void setEmpty();
+		void setFull();
+		void fill(uint32_t value);
+		void drain(uint32_t value);
+		uint32_t getCapacity() const;
+		uint32_t getValue() const;
+		uint32_t setValue() const;
+		void init(uint32_t capacity, uint32_t value);
+
+		Gauge& operator=(const Gauge &gauge);
+		Gauge& operator=(Gauge &&gauge);
+
+	private:
+		uint32_t _capacity;
+		uint32_t _value;
+};
+
+#endif
