4923726d9df8e29cf32fa73bb30d2cec6d31f24a
mlordey <maximelordey>
2020-11-23 13:49:08 +0100

    Implement Gauge::fill

--- game/src/utils/gauge.cpp
+++ game/src/utils/gauge.cpp
@@ -15,3 +15,12 @@ void Gauge::setEmpty() {
 void Gauge::setFull() {
 	_value = _capacity;
 }
+
+void Gauge::fill(uint32_t value) {
+	uint32_t estimated = _value + value;
+	if ( (estimated < _value) || (estimated > _capacity) ){
+		_value = _capacity;
+	} else {
+		_value += value;
+	}
+}
