ec68420a4fcd552a13fd62ccb7067f67bc2b96d8
mlordey <maximelordey>
2020-11-23 13:29:07 +0100

    add const keyword

--- game/src/utils/gauge.cpp
+++ game/src/utils/gauge.cpp
@@ -1,9 +1,9 @@
 #include "gauge.h"
 
-bool Gauge::isEmpty() {
+bool Gauge::isEmpty() const {
 	return _value == 0;
 }
 
-bool Gauge::isFull() {
+bool Gauge::isFull() const {
 	return _value == _capacity;
 }
