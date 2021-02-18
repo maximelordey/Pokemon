1f2ed426fa36c68ee63da28e25b3f56e3e911eb9
mlordey <maximelordey>
2021-02-18 12:50:56 +0100

    Create class Iv.

---
+++ game/main/monster/include/iv.h
@@ -0,0 +1,17 @@
+#ifndef IV_H
+#define IV_H
+
+#include <cstdint> 
+
+class Iv {
+	
+	public:
+		uint32_t _health;
+		uint32_t _attack;
+		uint32_t _defence;
+		uint32_t _attackSpe;
+		uint32_t _defenceSpe;
+		uint32_t _speed;
+};
+
+#endif
---
+++ game/main/monster/src/iv.cpp
@@ -0,0 +1,1 @@
+#include "iv.h"
