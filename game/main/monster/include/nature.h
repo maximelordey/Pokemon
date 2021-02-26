599ffd0dd03ff64586fccbed9855c0ba5aad7e14
mlordey <maximelordey>
2021-02-26 10:51:25 +0100

    Complete Nature's interface.

---
+++ game/main/monster/include/nature.h
@@ -0,0 +1,21 @@
+#ifndef NATURE_H
+#define NATURE_H
+
+#include <string>
+#include "coefficient.h"
+
+class Nature {
+	Nature();
+	Nature(const char* name);
+	Nature(const std::string name);
+
+	public:
+		std::string _name;
+		Coefficient _attack;
+		Coefficient _defence;
+		Coefficient _attackSpe;
+		Coefficient _defenceSpe;
+		Coefficient _speed;
+};
+
+#endif
