81de4d5878779934530aaea1cb7e36ad48d90592
mlordey <maximelordey>
2021-03-01 12:02:45 +0100

    Complete class Ev.

--- game/main/monster/include/ev.h
+++ game/main/monster/include/ev.h
@@ -9,21 +9,74 @@ class Ev {
 		Ev();
 		Ev(uint32_t total_limit, uint32_t stat_limit);
 
-		void setHealth(uint32_t value);
-		void setAttack(uint32_t value);
-		void setDefence(uint32_t value);
-		void setAttackSpe(uint32_t value);
-		void setDefenceSPe(uint32_t value);
-		void setSpeed(uint32_t value);
+		bool isHealthEmpty() const;
+		bool isHealthFull() const;
+		void setHealthEmpty();
+		void setHealthFull();
+		void addHealth(uint32_t value);
+		void removeHealth(uint32_t value);
+		uint32_t getHealthCapacity() const;
+		uint32_t getHealthValue() const;
+		void setHealthValue(uint32_t value);
+		void initHealth(uint32_t capacity, uint32_t value);
 
-		uint32_t getHealth() const;
-		uint32_t getAttack() const;
-		uint32_t getDefence() const;
-		uint32_t getAttackSpe() const;
-		uint32_t getDefenceSpe() const;
-		uint32_t getSpeed() const;
+		bool isAttackEmpty() const;
+		bool isAttackFull() const;
+		void setAttackEmpty();
+		void setAttackFull();
+		void addAttack(uint32_t value);
+		void removeAttack(uint32_t value);
+		uint32_t getAttackCapacity() const;
+		uint32_t getAttackValue() const;
+		void setAttackValue(uint32_t value);
+		void initAttack(uint32_t capacity, uint32_t value);
+
+		bool isDefenceEmpty() const;
+		bool isDefenceFull() const;
+		void setDefenceEmpty();
+		void setDefenceFull();
+		void addDefence(uint32_t value);
+		void removeDefence(uint32_t value);
+		uint32_t getDefenceCapacity() const;
+		uint32_t getDefenceValue() const;
+		void setDefenceValue(uint32_t value);
+		void initDefence(uint32_t capacity, uint32_t value);
+
+		bool isAttackSpeEmpty() const;
+		bool isAttackSpeFull() const;
+		void setAttackSpeEmpty();
+		void setAttackSpeFull();
+		void addAttackSpe(uint32_t value);
+		void removeAttackSpe(uint32_t value);
+		uint32_t getAttackSpeCapacity() const;
+		uint32_t getAttackSpeValue() const;
+		void setAttackSpeValue(uint32_t value);
+		void initAttackSpe(uint32_t capacity, uint32_t value);
+
+		bool isDefenceSpeEmpty() const;
+		bool isDefenceSpeFull() const;
+		void setDefenceSpeEmpty();
+		void setDefenceSpeFull();
+		void addDefenceSpe(uint32_t value);
+		void removeDefenceSpe(uint32_t value);
+		uint32_t getDefenceSpeCapacity() const;
+		uint32_t getDefenceSpeValue() const;
+		void setDefenceSpeValue(uint32_t value);
+		void initDefenceSpe(uint32_t capacity, uint32_t value);
+
+		bool isSpeedEmpty() const;
+		bool isSpeedFull() const;
+		void setSpeedEmpty();
+		void setSpeedFull();
+		void addSpeed(uint32_t value);
+		void removeSpeed(uint32_t value);
+		uint32_t getSpeedCapacity() const;
+		uint32_t getSpeedValue() const;
+		void setSpeedValue(uint32_t value);
+		void initSpeed(uint32_t capacity, uint32_t value);
 
 		uint32_t getTotalStats() const;
+		bool isFull();
 
 	private:
 		Gauge _health;
--- game/main/monster/src/ev.cpp
+++ game/main/monster/src/ev.cpp
@@ -11,55 +11,284 @@ Ev::Ev(uint32_t total_limit, uint32_t stat_limit)
 	 _attackSpe(stat_limit), _defenceSpe(stat_limit), _speed(stat_limit), _total_limit(total_limit)
 {}
 
-void Ev::setHealth(uint32_t value) {
-	uint32_t total_left = _total_limit - this->getTotalStats();
-	uint32_t value_left = std::min(value,  total_left);
-
-	_health.add(value_left);
+bool Ev::isHealthEmpty() const {
+	return _health.isEmpty();
 }
 
-void Ev::setAttack(uint32_t value) {
-
+bool Ev::isHealthFull() const {
+	return _health.isFull();
 }
 
-void Ev::setDefence(uint32_t value) {
-
+void Ev::setHealthEmpty() {
+	return _health.setEmpty();
 }
 
-void Ev::setAttackSpe(uint32_t value) {
+void Ev::setHealthFull() {
 
 }
 
-void Ev::setDefenceSPe(uint32_t value) {
-
+void Ev::addHealth(uint32_t value) {
+	uint32_t total_gap = _total_limit - this->getTotalStats();
+	uint32_t final_value = std::min(value, total_gap);
+	_health.add(final_value);
 }
 
-void Ev::setSpeed(uint32_t value) {
-
+void Ev::removeHealth(uint32_t value) {
+	_health.remove(value);
 }
 
-uint32_t Ev::getHealth() const {
+uint32_t Ev::getHealthCapacity() const {
+	return _health.getCapacity();
+}
+
+uint32_t Ev::getHealthValue() const {
 	return _health.getValue();
 }
 
-uint32_t Ev::getAttack() const {
+void Ev::setHealthValue(uint32_t value) {
+	uint32_t total_without_health = this->getTotalStats() - _health.getValue();
+	uint32_t total_gap = _total_limit - total_without_health;
+	uint32_t final_value = std::min(value, total_gap);
+	_health.setValue(final_value);
+}
+
+void Ev::initHealth(uint32_t capacity, uint32_t value) {
+	_health.init(capacity, value);
+}
+
+
+bool Ev::isAttackEmpty() const {
+	return _attack.isEmpty();
+}
+
+bool Ev::isAttackFull() const {
+	return _attack.isFull();
+}
+
+void Ev::setAttackEmpty() {
+	_attack.setEmpty();
+}
+
+void Ev::setAttackFull() {
+
+}
+
+void Ev::addAttack(uint32_t value) {
+	uint32_t total_gap = _total_limit - this->getTotalStats();
+	uint32_t final_value = std::min(value, total_gap);
+	_attack.add(final_value);
+}
+
+void Ev::removeAttack(uint32_t value) {
+	_attack.remove(value);
+}
+
+uint32_t Ev::getAttackCapacity() const {
+	return _attack.getCapacity();
+}
+
+uint32_t Ev::getAttackValue() const {
 	return _attack.getValue();
 }
 
-uint32_t Ev::getDefence() const {
+void Ev::setAttackValue(uint32_t value) {
+	uint32_t total_without_attack = this->getTotalStats() - _attack.getValue();
+	uint32_t total_gap = _total_limit - total_without_attack;
+	uint32_t final_value = std::min(value, total_gap);
+	_attack.setValue(final_value);
+}
+
+void Ev::initAttack(uint32_t capacity, uint32_t value) {
+	_attack.init(capacity, value);
+}
+
+
+bool Ev::isDefenceEmpty() const {
+	return _defence.isEmpty();
+}
+
+bool Ev::isDefenceFull() const {
+	return _defence.isFull();
+}
+
+void Ev::setDefenceEmpty() {
+	_defence.setEmpty();
+}
+
+void Ev::setDefenceFull() {
+
+}
+
+void Ev::addDefence(uint32_t value) {
+	uint32_t total_gap = _total_limit - this->getTotalStats();
+	uint32_t final_value = std::min(value, total_gap);
+	_defence.add(final_value);
+}
+
+void Ev::removeDefence(uint32_t value) {
+	_defence.remove(value);
+}
+
+uint32_t Ev::getDefenceCapacity() const {
+	return _defence.getCapacity();
+}
+
+uint32_t Ev::getDefenceValue() const {
 	return _defence.getValue();
 }
 
-uint32_t Ev::getAttackSpe() const {
+void Ev::setDefenceValue(uint32_t value) {
+	uint32_t total_without_defence = this->getTotalStats() - _defence.getValue();
+	uint32_t total_gap = _total_limit - total_without_defence;
+	uint32_t final_value = std::min(value, total_gap);
+	_attack.setValue(final_value);
+}
+
+void Ev::initDefence(uint32_t capacity, uint32_t value) {
+	_defence.init(capacity, value);
+}
+
+
+bool Ev::isAttackSpeEmpty() const {
+	return _attackSpe.isEmpty();
+}
+
+bool Ev::isAttackSpeFull() const {
+	return _attackSpe.isFull();
+}
+
+void Ev::setAttackSpeEmpty() {
+	_attackSpe.setEmpty();
+}
+
+void Ev::setAttackSpeFull() {
+
+}
+
+void Ev::addAttackSpe(uint32_t value) {
+	uint32_t total_gap = _total_limit - this->getTotalStats();
+	uint32_t final_value = std::min(value, total_gap);
+	_attackSpe.add(final_value);
+}
+
+void Ev::removeAttackSpe(uint32_t value) {
+	_attackSpe.remove(value);
+}
+
+uint32_t Ev::getAttackSpeCapacity() const {
+	return _attackSpe.getCapacity();
+}
+
+uint32_t Ev::getAttackSpeValue() const {
 	return _attackSpe.getValue();
 }
 
-uint32_t Ev::getDefenceSpe() const {
+void Ev::setAttackSpeValue(uint32_t value) {
+	uint32_t total_without_attackSpe = this->getTotalStats() - _attackSpe.getValue();
+	uint32_t total_gap = _total_limit - total_without_attackSpe;
+	uint32_t final_value = std::min(value, total_gap);
+	_attack.setValue(final_value);
+}
+
+void Ev::initAttackSpe(uint32_t capacity, uint32_t value) {
+	_attackSpe.init(capacity, value);
+}
+
+
+bool Ev::isDefenceSpeEmpty() const {
+	return _defenceSpe.isEmpty();
+}
+
+bool Ev::isDefenceSpeFull() const {
+	return _defenceSpe.isFull();
+}
+
+void Ev::setDefenceSpeEmpty() {
+	_defenceSpe.setEmpty();
+}
+
+void Ev::setDefenceSpeFull() {
+
+}
+
+void Ev::addDefenceSpe(uint32_t value) {
+	uint32_t total_gap = _total_limit - this->getTotalStats();
+	uint32_t final_value = std::min(value, total_gap);
+	_defenceSpe.add(final_value);
+}
+
+void Ev::removeDefenceSpe(uint32_t value) {
+	_defenceSpe.remove(value);
+}
+
+uint32_t Ev::getDefenceSpeCapacity() const {
+	return _defenceSpe.getCapacity();
+}
+
+uint32_t Ev::getDefenceSpeValue() const {
 	return _defenceSpe.getValue();
 }
 
-uint32_t Ev::getSpeed() const {
+void Ev::setDefenceSpeValue(uint32_t value) {
+	uint32_t total_without_defenceSpe = this->getTotalStats() - _defenceSpe.getValue();
+	uint32_t total_gap = _total_limit - total_without_defenceSpe;
+	uint32_t final_value = std::min(value, total_gap);
+	_attack.setValue(final_value);
+}
+
+void Ev::initDefenceSpe(uint32_t capacity, uint32_t value) {
+	_defenceSpe.init(capacity, value);
+}
+
+
+bool Ev::isSpeedEmpty() const {
+	_speed.isEmpty();
+}
+
+bool Ev::isSpeedFull() const {
+	_speed.isFull();
+}
+
+void Ev::setSpeedEmpty() {
+	_speed.setEmpty();
+}
+
+void Ev::setSpeedFull() {
+
+}
+
+void Ev::addSpeed(uint32_t value) {
+	uint32_t total_gap = _total_limit - this->getTotalStats();
+	uint32_t final_value = std::min(value, total_gap);
+	_speed.add(final_value);
+}
+
+void Ev::removeSpeed(uint32_t value) {
+	_speed.remove(value);
+}
+
+uint32_t Ev::getSpeedCapacity() const {
+	return _speed.getCapacity();
+}
+
+uint32_t Ev::getSpeedValue() const {
 	return _speed.getValue();
+}
+
+void Ev::setSpeedValue(uint32_t value) {
+	uint32_t total_without_speed = this->getTotalStats() - _speed.getValue();
+	uint32_t total_gap = _total_limit - total_without_speed;
+	uint32_t final_value = std::min(value, total_gap);
+	_attack.setValue(final_value);
+}
+
+void Ev::initSpeed(uint32_t capacity, uint32_t value) {
+	_speed.init(capacity, value);
+}
+
+
+bool Ev::isFull() {
+	return this->getTotalStats() == _total_limit;
 }
 
 uint32_t Ev::getTotalStats() const {
