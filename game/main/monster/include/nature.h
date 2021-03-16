3c355bb7be87b5a14445023bf4955d27914e8c07
mlordey <maximelordey>
2021-03-16 15:47:38 +0100

    Class Nature --> struct Nature.

--- game/main/monster/include/nature.h
+++ game/main/monster/include/nature.h
@@ -1,28 +1,18 @@
 #ifndef NATURE_H
 #define NATURE_H
 
-#include <string>
-
 #include "coefficient.h"
 #include "identifier.h"
 #include "translation.h"
 
-class Nature {
-	public:
-		static const float DEFAULT_VALUE;
-
-	public:
-		Nature();
-		Nature(const Identifier& identifier);
-
-	public:
-		Identifier _identifier;
-		Translation _name;
-		Coefficient _attack;
-		Coefficient _defence;
-		Coefficient _attackSpe;
-		Coefficient _defenceSpe;
-		Coefficient _speed;
-};
+typedef struct {
+	Identifier _identifier;
+	Translation _name;
+	Coefficient _attack;
+	Coefficient _defence;
+	Coefficient _attackSpe;
+	Coefficient _defenceSpe;
+	Coefficient _speed;
+} Nature;
 
 #endif
