75fc7c42d5f8207be2d834d455a3689a8d4c569c
mlordey <maximelordey>
2021-03-16 15:55:08 +0100

    Add struct Weaknesses. Add Weaknesses to struct Type.

--- game/main/monster/include/type.h
+++ game/main/monster/include/type.h
@@ -1,28 +1,15 @@
 #ifndef TYPE_H
 #define TYPE_H
 
-#include <string>
-#include <map>
-
+#include "weaknesses.h"
 #include "coefficient.h"
 #include "identifier.h"
 #include "translation.h"
 
-class Type {
-	public:
-		Type();
-		Type(const Identifier &identifier);
-
-		bool isEqualTo(const Type &type) const;
-		bool operator==(const Type &type) const;
-		bool operator!=(const Type &type) const;
-		bool operator<(const Type &type) const;
-		bool operator>(const Type &type) const;
-
-	public:
-		Identifier _identifier;
-		Translation _name;
-		std::map<Type, Coefficient> _weaknesses;
-};
+typedef struct {
+	Identifier _identifier;
+	Translation _name;
+	Weaknesses _weaknesses;
+} Type;
 
 #endif
