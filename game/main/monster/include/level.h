fe6ad13f060d4b0fba1caa9d48fdd79aef110f66
mlordey <maximelordey>
2021-03-16 15:46:30 +0100

    Class Level --> struct Level.

--- game/main/monster/include/level.h
+++ game/main/monster/include/level.h
@@ -1,16 +1,10 @@
 #ifndef LEVEL_H
 #define LEVEL_H
 
-#include <cstdint>
 #include "gauge.h"
 
-class Level {
-	public:
-		Level();
-		Level(uint32_t levelMax);
-
-	public:
-		Gauge _gauge;
-};
+typedef struct {
+	Gauge _gauge;
+} Level;
 
 #endif
--- game/main/monster/src/level.cpp
+++
@@ -1,9 +0,0 @@
-#include "level.h"
-
-Level::Level()
-	: _gauge()
-{}
-
-Level::Level(uint32_t levelMax)
-	: _gauge(levelMax)
-{}
