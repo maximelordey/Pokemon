a9a896d71f620f3991ac41309622978ace750b2b
mlordey <maximelordey>
2021-04-02 21:11:07 +0200

    Create module input. Create module sound. Create module visual.

--- engine/include/action_command/iaction_command.h
+++
@@ -1,8 +0,0 @@
-#ifndef IACTION_COMMAND_H
-#define IACTION_COMMAND_H
-
-class IActionCommand {
-	public:
-		virtual void action() = 0;
-};
-#endif
--- engine/include/audio_manager/iaudio_file.h
+++
@@ -1,6 +0,0 @@
-#ifndef IAUDIO_FILE_H
-#define IAUDIO_FILE_H
-
-class IAudioFile {};
-
-#endif
