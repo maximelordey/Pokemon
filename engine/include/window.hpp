016ad474cbb55f480515036aea7971d61fe36e05
mlordey <maximelordey>
2021-04-11 18:24:18 +0200

    Create class Window. Create struct WindowCreateInfo. Create typedef WindowFlags.

---
+++ engine/include/window.hpp
@@ -0,0 +1,34 @@
+#ifndef WINDOW_HPP
+#define WINDOW_HPP
+
+#include <string>
+#include <SDL2/SDL.h>
+#include "point.hpp"
+#include "dimension.hpp"
+
+typedef uint32_t WindowFlags;
+
+class Window {
+	public:
+		Window();
+		Window(const WindowCreateInfo& infos);
+		~Window();
+		Window(const Window& window);
+		Window(Window&& window);
+
+		const SDL_Window* get() const;
+
+		Window& operator=(const Window& window);
+		Window& operator=(Window&& window);
+	private:
+		SDL_Window* window;
+};
+
+typedef struct WindowCreateInfo {
+	std::wstring name;
+	WindowFlags flags;
+	Point origin;
+	Dimension dimension;
+};
+
+#endif
